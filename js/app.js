cd1b91781dbad5ff2ec632f4d5365cc9126ea5be
MiauDona <donanferyt@gmail.com>
2023-12-11 13:48:33 +0100

    Cambio pequeño notable

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+// Get the button element
+const modoClaroOscuroButton = document.querySelector('.modo-claro-oscuro');
+
+// Add a click event listener to the button
+modoClaroOscuroButton.addEventListener('click', () => {
+    // Toggle the 'dark-mode' class on the body element
+    document.body.classList.toggle('dark-mode');
+});
+
+
